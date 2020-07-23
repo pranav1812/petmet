3bb2e7227e670dc73af3d10529edc044b24d9017
pranav1812 <pranavvohra1812@yahoo.in>
2020-07-23 19:11:46 +0530

    basic connection

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import {db, auth} from './firebase'
+
+export class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h1> hello world </h1>
+      </div>
+    )
+  }
+}
+
+export default App
