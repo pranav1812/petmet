fe2527e127fefadd9895a2bab0789429e0c16975
pranav1812 <pranavvohra1812@yahoo.in>
2020-08-14 23:51:53 +0530

    backup1.2

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+//import 'firebase/analytics'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyASZKS4Sa6DsnAxJfCxh4P2v5PJG84WrbM",
+    authDomain: "petmet-268a4.firebaseapp.com",
+    databaseURL: "https://petmet-268a4.firebaseio.com",
+    projectId: "petmet-268a4",
+    storageBucket: "petmet-268a4.appspot.com",
+    messagingSenderId: "1082201721355",
+    appId: "1:1082201721355:web:930a4a9192253fef1ed50e",
+    measurementId: "G-6JBWLHV87K"
+}
+
+export const fire= firebase.initializeApp(firebaseConfig)
+// firebase.analytics()
+export const db= firebase.firestore()
+export const auth= firebase.auth()
+
