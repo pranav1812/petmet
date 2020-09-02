f9922612b93742cee75270f6825dae0fbe6861ac
sartajsehgal <sartajsehgal16@gmail.com>
2020-09-02 18:23:28 +0530

    profile errors solved

--- src/components/client/Profile.js
+++ src/components/client/Profile.js
@@ -9,7 +9,7 @@ import {Router, Link} from 'react-router-dom';
 export default function EditProfile() {
 
   const [uid, setUid]= useState(null)
-
+  const [client,setClient]=useState(null)
   const [profile, setProfile]= useState({
     name: null,
     mail: null,
@@ -26,19 +26,21 @@ export default function EditProfile() {
         setUid(user.uid)
         db.collection('user').doc(user.uid).get()
           .then(doc=>{
-            if(!doc.exists){
-              alert("some error occured")
-            }
-          })
+            if(doc.exists){
+             // docs.forEach(doc=>{
+              setProfile(doc.data())
+              setClient(doc.data())}
+          }
+          )
       }
     })
     
   },[])
 
   const submit=()=>{
-    const {name, mail, phone, address, city, state, zip}= profile
     db.collection('user').doc(uid).update({
       ...profile,
+      profileCompleted: true,
     }).then(()=>{
       window.location= window.location.protocol + "//" + window.location.host + "/" +'Home/'
     })
@@ -50,156 +52,83 @@ export default function EditProfile() {
     <React.Fragment>
       <h1 className="main-head mt-4">UPDATE YOUR PROFILE</h1>
         <div className="container m-4">
-                    <Form className="addProduct_form">
-                        <Form.Group className="row">
-                            <Form.Label className="col-3">Name</Form.Label>
-                            <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input" id="Name"
-                               name="Name" autoComplete="given-name"
-                               onBlur={e=>{setProfile({...profile, name: e.target.value})}}> 
-                               </Form.Control>
-                        </Form.Group>
-                        <Form.Group className="row">
-                            <Form.Label className="col-3">E-Mail</Form.Label>
-                            <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
-                            id="mail"
-                            name="mail"
-                            autoComplete="email"
-                            onBlur={e=>{setProfile({...profile, mail: e.target.value})}}> 
-                            </Form.Control>
-                            </Form.Group>
-                        <Form.Group className="row">
-                            <Form.Label className="col-3">Phone Number</Form.Label>
-                            <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
-                             id="phone"
-                             name="phone"
-                             autoComplete="number"
-                             onBlur={e=>{setProfile({...profile, number: e.target.value})}}> 
-                             </Form.Control>
-                        </Form.Group>
-                        <Form.Group className="row">
-                            <Form.Label className="col-3">Address</Form.Label>
-                            <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
-                             id="address"
-                             name="address"
-                             autoComplete="address"
-                             onBlur={e=>{setProfile({...profile, address: e.target.value})}}> 
-                             </Form.Control>
-                        </Form.Group>
-                        <Form.Group className="row">
-                            <Form.Label className="col-3">City</Form.Label>
-                            <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
-                             id="city"
-                             name="city"
-                             autoComplete="city"
-                             onBlur={e=>{setProfile({...profile, city: e.target.value})}}> 
-                             </Form.Control>
-                        </Form.Group>
-                        <Form.Group className="row">
-                            <Form.Label className="col-3">State</Form.Label>
-                            <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
-                             id="state"
-                             name="state"
-                             autoComplete="state"
-                             onBlur={e=>{setProfile({...profile, state: e.target.value})}}> 
-                             </Form.Control>
-                        </Form.Group>
-                        <Form.Group className="row">
-                            <Form.Label className="col-3">Zip</Form.Label>
-                            <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
-                             id="zip"
-                             name="zip"
-                             autoComplete="shipping postal-code"
-                             onBlur={e=>{setProfile({...profile, zip: e.target.value})}}> 
-                             </Form.Control>
-                        </Form.Group>
-                        <button type="button" className="offset-4 offset-sm-3 pink_out" onClick={submit}>
-                            Update
-                        </button>
-                    </Form>
+
+          <Form className="addProduct_form">
+          <Form.Group className="row">
+              <Form.Label className="col-3">Name</Form.Label>
+              <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input" id="Name"
+                 name="Name" autoComplete="given-name"
+                 placeholder={client?client.name:null}
+                 onBlur={e=>{setProfile({...profile, name: e.target.value})}}> 
+                 </Form.Control>
+          </Form.Group>
+          <Form.Group className="row">
+              <Form.Label className="col-3">E-Mail</Form.Label>
+              <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
+              id="mail"
+              name="mail"
+              autoComplete="email"
+              placeholder={client?client.mail:null}
+              onBlur={e=>{setProfile({...profile, mail: e.target.value})}}> 
+              </Form.Control>
+              </Form.Group>
+          <Form.Group className="row">
+              <Form.Label className="col-3">Phone Number</Form.Label>
+              <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
+               id="phone"
+               name="phone"
+               autoComplete="number"
+               placeholder={client?client.phone:null}
+               onBlur={e=>{setProfile({...profile, phone: e.target.value})}}> 
+               </Form.Control>
+          </Form.Group>
+          <Form.Group className="row">
+              <Form.Label className="col-3">Address</Form.Label>
+              <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
+               id="address"
+               name="address"
+               autoComplete="address"
+               placeholder={client?client.address:null}
+               onBlur={e=>{setProfile({...profile, address: e.target.value})}}> 
+               </Form.Control>
+          </Form.Group>
+          <Form.Group className="row">
+              <Form.Label className="col-3">City</Form.Label>
+              <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
+               id="city"
+               name="city"
+               autoComplete="city"
+               placeholder={client?client.city:null}
+               onBlur={e=>{setProfile({...profile, city: e.target.value})}}> 
+               </Form.Control>
+          </Form.Group>
+          <Form.Group className="row">
+              <Form.Label className="col-3">State</Form.Label>
+              <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
+               id="state"
+               name="state"
+               autoComplete="state"
+               placeholder={client?client.state:null}
+               onBlur={e=>{setProfile({...profile, state: e.target.value})}}> 
+               </Form.Control>
+          </Form.Group>
+          <Form.Group className="row">
+              <Form.Label className="col-3">Zip</Form.Label>
+              <Form.Control required className="col-7 col-sm-8 offset-sm-0 offset-1" as="input"
+               id="zip"
+               name="zip"
+               autoComplete="shipping postal-code"
+               placeholder={client?client.zip:null}
+              onBlur={e=>{setProfile({...profile, zip: e.target.value})}}> 
+               </Form.Control>
+          </Form.Group>
+          <button type="button" className="offset-4 offset-sm-3 pink_out" onClick={submit}>
+              Update
+          </button>
+      </Form>
+      
+
                 </div>
     </React.Fragment>
   );
 }
-
-{/**
-<Typography variant="h6" gutterBottom>
-        Update your Profile
-      </Typography>
-     
-        <Grid item xs={12}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="name"
-            name="name"
-            label="Full name"
-            fullWidth
-            autoComplete="given-name"
-            onBlur={e=>{ls.set('name', e.target.value)}}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="mail"
-            name="mail"
-            label="E-mail"
-            fullWidth
-            autoComplete="email"
-            onBlur={e=>{ls.set('mail', e.target.value)}}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="phone"
-            name="phone"
-            label="Phone Number"
-            fullWidth
-            autoComplete="number"
-            onBlur={e=>{ls.set('phone', e.target.value)}}
-          />
-        </Grid>
-            <Grid>
-          <TextField
-            required
-            id="address"
-            name="address"
-            label="Address"
-            fullWidth
-            autoComplete="address"
-            required
-            onBlur={e=>{ls.set('address', e.target.value)}}
-          />
-        </Grid>
-        
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="city"
-            onBlur={e=>{ls.set('city', e.target.value)}}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField id="state" name="state" label="State" fullWidth
-          onBlur={e=>{ls.set('state', e.target.value)}}
-          />
-          
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Pin code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            onBlur={e=>{ls.set('pin', e.target.value)}}
-          />
-        </Grid>
-        </Grid>
-*/}
