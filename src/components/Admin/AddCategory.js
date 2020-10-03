73fef36893f23b231083c3aff7d8ecb7dbede620
Ikshul Dureja <ikshuldureja130@gmail.com>
2020-10-03 23:48:23 +0530

    admin

--- src/components/Admin/AddCategory.js
+++ src/components/Admin/AddCategory.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Form } from 'react-bootstrap';
 
+import {db, auth, storage} from '../../firebase'
+
 const AddCategory = () => {
+    
+  const [uid, setUid]= useState(null)
+  const [url, setUrl]= useState(null)
+   const[cat, setCat]= useState({
+       category:null
+   })
+
+   useEffect(()=>{
+       auth.onAuthStateChanged(user=>{
+           if(user){
+               setUid(user.uid)
+               db.collection('items').doc(user.uid).get()
+               .then(doc=>{
+                   if(!doc.exists){
+                       alert("some error occured")
+                   }
+               })
+           }
+       })
+   },[])
+
+   
+  const addImg=(e)=>{
+    var file= e.target.files[0]
+    var storageRef= storage.ref('catImages/'+file.name)
+    storageRef.put(file).then(()=> {
+    alert("image uploaded")
+    storageRef.getDownloadURL()
+        .then(url=> setUrl(url))
+        .catch(err=> console.log(err))
+    })
+    .catch(err=> console.log(err))
+  }
+  
+  const submit=()=>{
+    var ref= db.collection('items').doc(uid)
+    const {category}= cat
+    ref.add({
+      ...cat,
+      url: url
+    }).then(()=>{
+      window.location.reload();
+    })
+    
+  }
+
+
     return ( 
         <div>
             <h1 className="main-head">ADD A NEW CATEGORY</h1>
@@ -9,13 +58,16 @@ const AddCategory = () => {
                     <Form className="addProduct_form">
                         <div className="row mb-3">
                             <Form.Label className="col-3">Image of Product</Form.Label>
-                            <input className="col-7 col-sm-8 offset-sm-0 offset-1" type="file"/>
+                            <input onChange={addImg} className="col-7 col-sm-8 offset-sm-0 offset-1" type="file"/>
+                            <img id="target"/>
                         </div>
                         <Form.Group className="row">
                             <Form.Label className="col-3">Name</Form.Label>
                             <Form.Control className="col-7 col-sm-8 offset-sm-0 offset-1" as="input" ></Form.Control>
+                            onBlur={e=>{setCat({...cat, category: e.target.value})}}
+                           
                         </Form.Group>
-                        <button type="button"  className="offset-4 offset-sm-3 pink_btn pink_btn_form">
+                        <button type="button"  className="offset-4 offset-sm-3 pink_btn pink_btn_form" onClick={submit}>
                             Add Category
                         </button>
                     </Form>
