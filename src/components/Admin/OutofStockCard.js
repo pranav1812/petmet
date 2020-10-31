8657dd13afcf4a944487e2340bd76369ecffbb82
pranav1812 <pranavvohra1812@yahoo.in>
2020-10-31 17:05:43 +0530

    admin connected

--- src/components/Admin/OutofStockCard.js
+++ src/components/Admin/OutofStockCard.js
@@ -1,7 +1,8 @@
 import React,{useState} from 'react';
 import {Card,Modal} from 'react-bootstrap';
 
-const OutCards = () => {
+const OutCards = (props) => {
+    const {data}= props
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -12,8 +13,8 @@ const OutCards = () => {
                                     <div className="row">
                                         <div className="col-12">
                                             <ul className="list-unstyled">
-                                                <li><strong>Product Name: </strong>Biscuits <br/></li>
-                                                <li><strong>Category: </strong>Dog Food <br/></li>
+                                                <li><strong>Product Name: </strong>{data.name} <br/></li>
+                                                <li><strong>Category: </strong>{data.category} <br/></li>
                                             </ul>
                                         </div>
                                     </div>
@@ -26,11 +27,14 @@ const OutCards = () => {
                 <Modal show={show} onHide={handleClose} centered>
                     <Modal.Body>
                         <ul className="list-unstyled">
+                            {/*
                             <li><strong>Product Name: </strong>Biscuits <br/></li>
                             <li><strong>Product Category: </strong>Dog Food <br /> </li>
                             <li><strong>Cost: </strong>Rs. 3000 <br/></li>
-                            <li><strong>Quantity: </strong>10 <br /></li>
+                            <li><strong>Quantity: </strong>10 <br /></li>*/ }
+                            
                         </ul>
+                        <li><strong>Product Id: </strong>{data.key} <br/></li>
                     </Modal.Body>
                     <Modal.Footer>
                         <button className="btn-sm pink_btn" onClick={handleClose}>
