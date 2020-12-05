58122c8274751eac053a2d077b73dfcc0cd09153
pranav1812 <pranavvohra1812@yahoo.in>
2020-12-06 03:22:06 +0530

    donno

--- functions/notifications/index.js
+++ functions/notifications/index.js
@@ -8,17 +8,22 @@ var notify={};
 
 // template file
 // docData= snap
-notify.sendToSingleUser= (deviceToken, docData, purposeId, coll, docId)=>{
+notify.sendToSingleUser= (deviceTokenSent, docData, purposeId, coll, docId)=>{
+    var deviceToken= deviceTokenSent || false
     var message= templates.generatePayload(docData, purposeId)
     message.token= deviceToken
     return new Promise((resolve, reject)=>{
-        var dateObj= new Date()
-        var dateStr= dateObj.toISOString()
-        var p1= fcm.send(message)
-        var p2= db.collection(coll).doc(docId).collection('notifications').doc(dateStr).set({message: message.notification.title})
-        //fcm.send(message).then((messageId)=>resolve(messageId))
-        resolve(Promise.all([p1, p2]))
-        .catch((error)=>reject(`error while sending notification to ${message.token}: ${error} `))
+        if(deviceToken){
+            var dateObj= new Date()
+            var dateStr= dateObj.toISOString()
+            var p1= fcm.send(message)
+            var p2= db.collection(coll).doc(docId).collection('notifications').doc(dateStr).set({message: message.notification.title})
+            //fcm.send(message).then((messageId)=>resolve(messageId))
+            resolve(Promise.all([p1, p2]))
+            .catch((error)=>reject(`error while sending notification to ${message.token}: ${error} `))
+        }
+        else resolve(69)
+        
     })
 }
 
