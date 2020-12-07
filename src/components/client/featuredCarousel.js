e2eabfaa481aa33de00642397a21e6f3f6605c57
Ikshul Dureja <ikshuldureja130@gmail.com>
2020-12-07 13:35:44 +0530

    featured carousel done

--- src/components/client/featuredCarousel.js
+++ src/components/client/featuredCarousel.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import SquareCard from './SquareCard';
 import Carousel from "react-elastic-carousel";
+import { db } from "../../firebase";
 
 const breakPoints = [
     { width: 1, itemsToShow: 1 },
@@ -11,6 +12,23 @@ const breakPoints = [
   ];
   
 const FeaturedCarousel = () => {
+
+    const [cats, setCats] = useState(null);
+    useEffect(()=>{
+        db.collection("items")
+        .doc("Cat Essentials")
+        .collection("products")
+        .get()
+        .then((docs) => {
+          var temp = [];
+          docs.forEach((doc) => temp.push({...doc.data(), key:doc.id }));
+          setCats(temp);
+          console.log(cats)
+          console.log(temp)
+        })
+        .catch((e) => console.log(e));
+    },[]);
+
     return ( 
         <div>
             <h2 className="mt-4" style={{ paddingBottom: "20px",fontWeight:"bold" }}>
@@ -21,19 +39,19 @@ const FeaturedCarousel = () => {
                 style={{ justifyContent: "center", paddingBottom: "40px" }}
             >
                 <Carousel breakPoints={breakPoints}>
-                <item>
-                    <SquareCard />
-                </item>
-                <item>
-                    <SquareCard />
-                </item>
-                <item>
-                    <SquareCard />
-                </item>
-                <item>
-                    <SquareCard />
-                </item>
-                </Carousel>
+
+                
+                {cats ? (
+                  cats.map((ca) => <item>       
+                  <SquareCard _id={ca.key} info={ca.details} title={ca.details.name} image={ca.details.url} size={ca.details.size} cost={ca.details.cost} mrp={ca.details.mrp}/>
+                  </item>
+                  )
+                 ) : (
+                   <h5>Cat Essentials arriving</h5>
+                 )
+                 }
+
+               </Carousel>
             </div>
         </div>
      );
